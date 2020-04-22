efae3228a9fd7a67fc85497eb52994c4b2ee78d0
Nick <Nicholas.Celiberti@gmail.com>
2020-04-22 00:00:57 -0400

    minimally finished

--- app/clueGame.js
+++ app/clueGame.js
@@ -34,7 +34,7 @@ function movementAction(el) {
     let currTurn = rtn['currentTurn']
     let playerNum = 'p' + document.getElementById(`playerID`).innerHTML
 
-    getCallback('/location', function(rtn) {
+    getCallback('/data', function(rtn) {
       let from = rtn['locations'][currTurn]
       if (movementAllowed(el, currTurn, from, rtn['moved'], rtn['started'])) {
         $.post('/move', {
@@ -98,6 +98,12 @@ function movementAllowed(to, currTurn, from, moved, started) {
     return false
 }
 
+function show(player, card) {
+  $.post('/privateMessage', {
+    javascript_data: JSON.stringify({'message': 'Player ' + player + ' showed you ' + card})
+  })
+}
+
 function displayMessage(message) {
   $.post('/message', {
     javascript_data: JSON.stringify({'message':message})
@@ -126,7 +132,6 @@ function endTurn(id) {
       else
           nextPlayerNum = playerNum + 1;
 
-
       displayMessage(`Player ${playerNum} ended their turn`);
     }
   })
@@ -134,7 +139,8 @@ function endTurn(id) {
 }
 
 var msgidx = 0
-
+var prividx = 0
+var addCards = true
 function update() {
   getCallback('/update', function(rtn) {
     let currTurn = rtn['currentTurn']
@@ -146,36 +152,93 @@ function update() {
       document.getElementById(locations[p]).innerHTML += `<img id=${p} class='playerImg' src='app/img/${p}.png'/>`
     }
     document.getElementById('currentTurn').innerHTML = `Player ${rtn['currentTurn']}`;
+
     for (let idx = msgidx; idx < rtn['messages'].length; idx++) {
       document.getElementById("textarea").innerHTML += rtn['messages'][idx] + '&#13;&#10;';
       msgidx+=1
     }
+    for (let idx = prividx; idx < rtn['privateMessages']['p' + document.getElementById("playerID").innerHTML].length; idx++) {
+      document.getElementById("textarea").innerHTML += rtn['privateMessages']['p' + document.getElementById("playerID").innerHTML][idx] + '&#13;&#10;';
+      prividx+=1
+    }
 
+    if(addCards) {
+      for (let idx = 0; idx < rtn['cards']['p' + document.getElementById("playerID").innerHTML].length; idx++) {
+        let text = rtn['cards']['p' + document.getElementById("playerID").innerHTML][idx]
+        document.getElementById('cards').innerHTML += `<button onclick="show(${document.getElementById("playerID").innerHTML},\'${text}\')">${text}</button><br/>`
+        addCards = false
+      }
+
+    }
   })
 }
 
-function makeAccusation(playerNum) {
-    displayMessage(`Player ${playerNum} made an accusation`);
+var rooms = ['study', 'hall', 'lounge', 'library', 'billiard', 'dining', 'conservatory', 'ballroom', 'kitchen']
+var charToId = {'Miss Scarlet':'p1',
+               'Col. Mustard':'p2',
+               'Mrs. White':'p3',
+               'Mr. Green':'p4',
+               'Mrs. Peacock':'p5',
+               'Prof. Plum':'p6' }
+
+function makeAccusation() {
+  getCallback('/data', function(rtn) {
+    let currTurn = rtn['currentTurn']
+    let location = rtn['locations']['p' + currTurn].substring(2)
+    let accused = document.querySelector('input[name="character"]:checked').value
+    let room = document.querySelector('input[name="room"]:checked').value
+    let weapon = document.querySelector('input[name="weapon"]:checked').value
+
+
+    if (document.getElementById(`playerID`).innerHTML != currTurn) {
+      alert("It is not your turn");
+      return false;
+    }
+    if (!rtn['started']) {
+      alert('Game has not started')
+      return false
+    }
+    displayMessage(`Player ${currTurn} says \"I accuse ${accused} of committing the crime in the ${room} with the ${weapon}.\"`);
+
+    $.post('/accuse', {
+      javascript_data: JSON.stringify({'accused':accused, 'room':room, 'weapon':weapon})
+    })
+    update()
+
+  })
+
 }
 
 function makeSuggestion(playerNum) {
-    displayMessage(`Player ${playerNum} made a suggestion`);
+
+  getCallback('/data', function(rtn) {
+    let currTurn = rtn['currentTurn']
+    let location = rtn['locations']['p' + currTurn].substring(2)
+    let accused = document.querySelector('input[name="character"]:checked').value
+    let weapon = document.querySelector('input[name="weapon"]:checked').value
+    if(rooms.includes(location)){
+      if (document.getElementById(`playerID`).innerHTML != currTurn) {
+        alert("It is not your turn");
+        return false;
+      }
+      if (!rtn['started'])  {
+        alert('Game has not started')
+        return false
+      }
+
+      displayMessage(`Player ${currTurn} says \"I suggest the crime was committed in the ${location} by ${accused} with the ${weapon}.\"`);
+
+      $.post('/move', {
+        javascript_data: JSON.stringify({'player':charToId[accused], 'to':rtn['locations']['p' + currTurn]})
+      })
+    } else {
+      alert('You are not in a room')
+    }
+    update()
+
+  })
+
 }
-
-function win() {
-
-}
-
-
-//Room Functions
-function getCoordinates() {
-
-}
-
-function hasSecretPassage() {
-
-}
-
 
 window.setInterval(function(){
   update()
--- clue.py
+++ clue.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 import json
+import random
+
 app = Flask(__name__, static_url_path='/app', static_folder='app')
 
 # Globals
@@ -7,10 +9,17 @@ playerCount = 0
 currentPlayerTurn = 1
 moved = False
 gameStarted = False
-messages = []
 
 # temps until database gets finished
 locations = {'p1':'45hallway2', 'p2':'54hallway5', 'p3':'41hallway12', 'p4':'21hallway11', 'p5':'12hallway8', 'p6':'14hallway3'}
+playerCards = {'p1':[], 'p2':[], 'p3':[], 'p4':[], 'p5':[], 'p6':[]}
+
+rooms = ['Study', 'Hall', 'Lounge', 'Library', 'Billiard', 'Dining', 'Conservatory', 'Ballroom', 'Kitchen']
+characters = ['Miss Scarlet', 'Col. Mustard', 'Mrs. White', 'Mr. Green', 'Mrs. Peacock', 'Prof. Plum']
+weapons = ['Candlestick', 'Revolver', 'Knife', 'Pipe', 'Wrench', 'Rope']
+crime = {}
+messages = []
+privateMessages = {'p1':[], 'p2':[], 'p3':[], 'p4':[], 'p5':[], 'p6':[]}
 
 @app.route('/')
 def root():
@@ -52,6 +61,17 @@ def move():
     moved = True
     return 'Ok', 200
 
+@app.route('/accuse', methods=['POST'])
+def accuse():
+    data = json.loads(request.form['javascript_data'])
+    if data == crime:
+        # Win
+        messages.append(f'Congradulations! Player {currentPlayerTurn} has won the game!')
+    else:
+        messages.append(f'Player {currentPlayerTurn} was incorrect.')
+    return 'Ok', 200
+
+
 @app.route('/message', methods=['POST'])
 def message():
     data = json.loads(request.form['javascript_data'])
@@ -59,19 +79,52 @@ def message():
     messages.append(data['message'])
     return 'Ok', 200
 
+@app.route('/privateMessage', methods=['POST'])
+def privateMessage():
+    data = json.loads(request.form['javascript_data'])
+    global privateMessages
+    privateMessages['p' + str(currentPlayerTurn)].append(data['message'])
+    return 'Ok', 200
+
 @app.route('/update', methods=['GET'])
 def update():
-    return jsonify({'currentTurn': currentPlayerTurn, 'locations': locations, 'messages':messages})
+    return jsonify({'currentTurn': currentPlayerTurn, 'locations': locations, 'messages':messages, 'cards':playerCards, 'privateMessages':privateMessages})
 
-@app.route('/location', methods=['GET'])
-def location():
-    return jsonify({'locations': locations, 'moved':moved, 'started':gameStarted})
+@app.route('/data', methods=['GET'])
+def data():
+    return jsonify({'locations': locations, 'moved':moved, 'started':gameStarted, 'currentTurn':currentPlayerTurn})
 
 @app.route('/start', methods=['POST'])
 def start():
-    #if(playerCount >= 3 and not gameStarted):
     global gameStarted
+    #if(playerCount >= 3 and not gameStarted):
     if(True and not gameStarted):
+        random.shuffle(rooms)
+        random.shuffle(characters)
+        random.shuffle(weapons)
+        global crime
+        global playerCards
+        crime = {'accused':characters.pop(), 'room':rooms.pop(), 'weapon':weapons.pop()}
+        print(crime)
+        count = 1
+        for i in range(len(characters)):
+            playerCards['p'+str(count)].append(characters[i])
+            count += 1
+            if (count == playerCount +1):
+                count = 1
+
+        for i in range(len(rooms)):
+            playerCards['p'+str(count)].append(rooms[i])
+            count += 1
+            if (count == playerCount +1):
+                count = 1
+
+        for i in range(len(weapons)):
+            playerCards['p'+str(count)].append(weapons[i])
+            count += 1
+            if (count == playerCount +1):
+                count = 1
+
         gameStarted = True
         global messages
         messages.append('Game started!')
